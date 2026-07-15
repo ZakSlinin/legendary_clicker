08a7d5a51248abb116f0817c438976fbc5ca3ec2
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2026-07-15 16:53:22 +0300

    feat: http.HandleFunc("/register", registerHandler)       	http.HandleFunc("/tap", tapHandler)       	http.HandleFunc("/balance", balanceHandler)

---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"log"
+	_ "modernc.org/sqlite"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+var (
+	db *sql.DB
+	mu sync.Mutex
+)
+
+func initDB() {
+	var err error
+	db, err = sql.Open("sqlite", "clicker.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY,
+			balance INTEGER NOT NULL DEFAULT 0
+		)
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		UserID int64 `json:"user_id"`
+	}
+	json.NewDecoder(r.Body).Decode(&req)
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	_, err := db.Exec(`
+		INSERT INTO users (id, balance) VALUES (?, 0)
+		ON CONFLICT(id) DO NOTHING
+	`, req.UserID)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var balance int64
+	db.QueryRow(`SELECT balance FROM users WHERE id = ?`, req.UserID).Scan(&balance)
+
+	json.NewEncoder(w).Encode(map[string]int64{"balance": balance})
+}
+
+func tapHandler(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		UserID    int64 `json:"user_id"`
+		TapCounts int64 `json:"tap_counts"`
+	}
+	json.NewDecoder(r.Body).Decode(&req)
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	_, err := db.Exec(`INSERT INTO users (id, balance) VALUES (?, ?)
+	ON CONFLICT(id) DO UPDATE SET balance = balance + ?
+	`, req.UserID, req.TapCounts, req.TapCounts)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var balance int64
+	db.QueryRow(`SELECT balance FROM users WHERE id = ?`, req.UserID).Scan(&balance)
+
+	json.NewEncoder(w).Encode(map[string]int64{"balance": balance})
+}
+
+func balanceHandler(w http.ResponseWriter, r *http.Request) {
+	userID, err := strconv.ParseInt(r.URL.Query().Get("user_id"), 10, 64)
+
+	if err != nil {
+		http.Error(w, "invalid user id", http.StatusInternalServerError)
+		return
+	}
+
+	var balance int64
+	err = db.QueryRow(`SELECT balance FROM users WHERE id = ?`, userID).Scan(&balance)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			http.Error(w, "user not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]int64{"balance": balance})
+}
+
+func main() {
+	initDB()
+
+	http.HandleFunc("/register", registerHandler)
+	http.HandleFunc("/tap", tapHandler)
+	http.HandleFunc("/balance", balanceHandler)
+
+	log.Println("server listening on localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
